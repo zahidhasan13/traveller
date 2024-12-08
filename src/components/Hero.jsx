527abf4d2d0e14746bc8acc6df1201240d314338
zahidhasan13 <zahidhasanofficial13@gmail.com>
2024-12-08 22:20:37 +0600

    hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,31 @@
 import { useRef } from "react";
-import { useHeroShutterReveil } from "../hooks/gsapAnimation";
+import heroImg1 from "../assets/sq1.jpg";
+import heroImg2 from "../assets/sq2.jpg";
+import heroImg3 from "../assets/sq3.jpg";
+import heroImg4 from "../assets/sq4.jpg";
+import heroImg5 from "../assets/sq5.jpg";
+import {
+  useHeroPhotoDropping,
+  useHeroShutterReveil,
+  useHeroPhotoLevitate,
+} from "../hooks/gsapAnimation";
 
 const Hero = () => {
   const heroRef = useRef(null);
   const shutter1 = useRef(null);
   const shutter2 = useRef(null);
+  const photo1 = useRef(null);
+  const photo2 = useRef(null);
+  const photo3 = useRef(null);
+  const photo4 = useRef(null);
+  const photo5 = useRef(null);
+
+  const heroPhotos = [photo1, photo2, photo3, photo4, photo5];
+
   useHeroShutterReveil(shutter1, 0, heroRef);
   useHeroShutterReveil(shutter2, 0.2, heroRef);
+  useHeroPhotoDropping(heroPhotos);
+  useHeroPhotoLevitate(heroPhotos, heroRef);
   return (
     <section className="hero container" ref={heroRef}>
       <h1 className="travel">
@@ -15,6 +34,33 @@ const Hero = () => {
       <h1 className="lover">
         lover <span ref={shutter2}></span>
       </h1>
+      <div className="photos">
+        <div
+          ref={photo1}
+          className="photo one"
+          style={{ backgroundImage: `url(${heroImg1})` }}
+        ></div>
+        <div
+          ref={photo2}
+          className="photo two"
+          style={{ backgroundImage: `url(${heroImg2})` }}
+        ></div>
+        <div
+          ref={photo3}
+          className="photo three"
+          style={{ backgroundImage: `url(${heroImg3})` }}
+        ></div>
+        <div
+          ref={photo4}
+          className="photo four"
+          style={{ backgroundImage: `url(${heroImg4})` }}
+        ></div>
+        <div
+          ref={photo5}
+          className="photo five"
+          style={{ backgroundImage: `url(${heroImg5})` }}
+        ></div>
+      </div>
     </section>
   );
 };
--- src/hooks/gsapAnimation.jsx
+++ src/hooks/gsapAnimation.jsx
@@ -17,9 +17,11 @@ export const useHeroShutterReveil = (item1, delay, trig) => {
         height: 0,
         duration: 1,
         delay: delay,
-        ease: Expo.easeIn,
+        ease: Expo.easeInOut,
         scrollTrigger: {
           trigger: trig.current,
+          start: "top center",
+          end: "bottom center",
           toggleActions: "play reverse play reverse",
         },
       }
@@ -42,6 +44,51 @@ export const useNavBarAnimation = (arr, delay = 0) => {
         duration: 0.5,
         delay: delay,
         stagger: 0.3,
+        ease: Expo.easeIn,
+      }
+    );
+  }, []);
+};
+
+export const useHeroPhotoDropping = (imgs) => {
+  useEffect(() => {
+    const el = imgs.map((img) => img.current);
+    gsap.fromTo(
+      el,
+      {
+        y: "-100vh",
+        opacity: 0,
+        scale: 0,
+      },
+      {
+        y: 0,
+        opacity: 1,
+        scale: 1,
+        duration: 1,
+        delay: 3,
+        stagger: 0.2,
+        ease: Expo.easeInOut,
+      }
+    );
+  }, []);
+};
+
+export const useHeroPhotoLevitate = (imgs, heroRef) => {
+  useEffect(() => {
+    const el = imgs.map((img) => img.current);
+    gsap.fromTo(
+      el,
+      {
+        y: 0,
+      },
+      {
+        y: "-40%",
+        ease: Expo.easeInOut,
+        scrollTrigger: {
+          trigger: heroRef.current,
+          scrub: 1,
+          toggleActions: "play reverse play reverse",
+        },
       }
     );
   }, []);
